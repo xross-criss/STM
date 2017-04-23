7a5993576e8c2c3d335988f501add292eadf06b1
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2017-04-23 11:15:47 +0200

    Utils added - Json Date-Time Serializer & Deserializer

---
+++ src/main/java/pl/morecraft/dev/stm/utils/JsonJodaLocalDateDeserializer.java
@@ -0,0 +1,32 @@
+package pl.morecraft.dev.stm.utils;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class JsonJodaLocalDateDeserializer extends JsonDeserializer<LocalDate> {
+
+    private static DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+    private static DateTimeFormatter formatterWithTime = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+        if (Objects.equals(jp.getText(), "")) {
+            return null;
+        }
+        try {
+            return LocalDate.parse(jp.getText(), formatter);
+        } catch (IllegalArgumentException e) {
+            return new LocalDate(LocalDateTime.parse(jp.getText(), formatterWithTime));
+        }
+    }
+
+}
+
---
+++ src/main/java/pl/morecraft/dev/stm/utils/JsonJodaLocalDateTimeDeserializer.java
@@ -0,0 +1,42 @@
+package pl.morecraft.dev.stm.utils;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class JsonJodaLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+    private static DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+    private static DateTimeFormatter formatterWithTime = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public LocalDateTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+        if (Objects.equals(jp.getText(), "")) {
+            return null;
+        }
+        try {
+            return LocalDateTime.parse(jp.getText(), formatterWithTime);
+        } catch (IllegalArgumentException e) {
+            LoggerFactory.getLogger(this.getClass()).error("", e);
+            LocalDate localDate = LocalDate.parse(jp.getText(), formatter);
+            return new LocalDateTime(
+                    localDate.getYear(),
+                    localDate.getMonthOfYear(),
+                    localDate.getDayOfMonth(),
+                    0,
+                    0,
+                    0
+            );
+        }
+    }
+
+}
+
